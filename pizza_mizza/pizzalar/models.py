463c08714a1cdf04e7318a294477db68b01383dc
Lala-Hashimli <hshml.lala@gmail.com>
2026-08-17 19:40:24 +0400

    update

--- pizza_mizza/pizzalar/models.py
+++ pizza_mizza/pizzalar/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 
 
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    size = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=6, decimal_places=2) 
+
+
+    def __str__(self):
+        return self.name
